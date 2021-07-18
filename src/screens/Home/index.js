742be9d01afdda763376a4128ef0cd341752bbfc
kyoudaiai <github@bikesolutions.org>
2021-07-18 14:43:57 +0200

    init

---
+++ src/screens/Home/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "./Hero";
+import Selection from "./Selection";
+import Popular from "./Popular";
+import HotBid from "../../components/HotBid";
+import Collections from "./Collections";
+import Discover from "./Discover";
+import Description from "./Description";
+
+const Home = () => {
+  return (
+    <>      
+      <Discover />      
+    </>
+  );
+};
+
+export default Home;
